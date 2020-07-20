7e63544f3a3b3f0f5c2104df2e340993da580c19
dilshanDD <55312516+dilshanDD@users.noreply.github.com>
2020-07-20 20:50:30 +0530

    heroes-client commit 20/07/2020

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+class Hero extends Component {
+    state = {heroId : 189 };                       
+    render(){
+      return (<React.Fragment>
+                   <h1> Avengers incoming ....!</h1>
+      <button> Click {this.state.heroId}</button> 
+              </React.Fragment>)
+    
+    }
+
+}               
+
+export default Hero;
